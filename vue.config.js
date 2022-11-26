1fa0f1c75aa1e7b028fcbc48ff5e3f02503514f6
psih2131 <gearsdevelope@gmail.com>
2022-11-26 20:11:51 +0300

    add fixed serve port

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    port: 8080,
     proxy: 'https://api.privatbank.ua'
   }
 })
